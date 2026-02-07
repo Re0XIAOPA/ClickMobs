43dc01e02a7abdb5306ac0d1d10dc39a85d40f20
Re0XIAOPA <mckekejun@outlook.com>
2026-02-08 01:06:29 +0800

    Localize_config_files

--- fabric/src/main/java/de/clickism/clickmobs/ClickMobsConfig.java
+++ fabric/src/main/java/de/clickism/clickmobs/ClickMobsConfig.java
@@ -24,9 +24,9 @@ public interface ClickMobsConfig {
                             """);
 
     ConfigOption<Boolean> CHECK_UPDATE =
-            CONFIG.option("check_update", true)
+            CONFIG.option("check_update", false)
                     .description("""
-                            Whether to check for updates on server startup. Strongly Recommended.
+                            是否在服务器启动时检查更新。
                             """);
 
     ConfigOption<List<String>> WHITELISTED_MOBS =
@@ -34,64 +34,64 @@ public interface ClickMobsConfig {
                     .listOf(String.class)
                     .header("""
                             ---------------------------------------------------------
-                            In the following section you can whitelist/blacklist mobs.
+                            在以下部分，您可以设置生物的白名单/黑名单。
                             ---------------------------------------------------------
-                            To whitelist/blacklist a vanilla mob, add its entity name.
-                                i.E: "creeper" or "ender_dragon"
-                            To whitelist/blacklist mobs from other mods, use the full identifier with the namespace.
-                                i.E: "othermod:fancy_creeper"
+                            要将原版生物加入白名单/黑名单，请添加其实体名称。
+                                例如: "creeper" 或 "ender_dragon"
+                            要将其他模组的生物加入白名单/黑名单，请使用带命名空间的完整标识符。
+                                例如: "othermod:fancy_creeper"
                             ---------------------------------------------------------
-                            You can also use tags (predicates) to whitelist/blacklist mobs with certain properties.
-                            Available tags are:
-                                ?all, ?hostile, ?baby, ?tamed, ?nametagged, ?silent, ?mob
+                            您还可以使用标签（谓词）来根据特定属性设置白名单/黑名单。
+                            可用的标签有：
+                                ?all (所有), ?hostile (敌对), ?baby (幼年), ?tamed (驯服), ?nametagged (命名), ?silent (静音), ?mob (特定生物)
                             
-                            You can (optionally) pass arguments to certain tags:
+                            您可以（可选地）向某些标签传递参数：
                                 - ?nametagged(Dinnerbone)
-                            You can combine multiple tags:
+                            您可以组合多个标签：
                                 - ?tamed ?nametagged
-                            You can negate tags using "not":
+                            您可以使用 "not" 来否定标签：
                                 - not ?hostile
-                            You can use the ?mob tag alongside other tags to use tags on specific mobs.
+                            您可以将 ?mob 标签与其他标签结合使用，以便在特定生物上使用标签。
                                 - ?mob(creeper, zombie) ?nametagged(Friendly!)
                             
-                            Check the wiki for more documentation on tags:
+                            查看 wiki 以获取更多关于标签的文档：
                             https://github.com/Clickism/ClickMobs/wiki/Tags
                             ---------------------------------------------------------
                             """)
                     .description("""
-                            Mobs that are allowed to be picked up.
-                            The whitelist takes precedence over the blacklist.
-                            (Blacklisted mobs included in the whitelist will still be allowed)
+                            允许被捡起的生物。
+                            白名单优先于黑名单。
+                            （包含在白名单中的黑名单生物仍然可以被捡起）
                             """);
 
     ConfigOption<List<String>> BLACKLISTED_MOBS =
             CONFIG.option("blacklisted_mobs", List.of("?hostile", "wither", "ender_dragon"))
                     .listOf(String.class)
                     .description("""
-                            Mobs that are not allowed to be picked up.
+                            不允许被捡起的生物。
                             """);
 
     ConfigOption<Boolean> ENABLE_DISPENSERS =
             CONFIG.option("enable_dispensers", true)
                     .description("""
-                            Whether dispensers can dispense picked up mobs.
+                            发射器是否可以发射捡起的生物。
                             """);
 
     ConfigOption<List<String>> BLACKLISTED_ITEMS_IN_HAND =
             CONFIG.option("blacklisted_items_in_hand", List.of("lead", "saddle"))
                     .listOf(String.class)
                     .description("""
-                            Items that prevent picking up mobs when held in hand.
-                            Use the (full) item identifiers of the items.
+                            手持时阻止捡起生物的物品。
+                            使用物品的（完整）标识符。
                             
-                            Harnesses are always blocked for Happy Ghast compatibility
-                            and do not need to be added here.
+                            为了兼容 Happy Ghast，缰绳总是被阻止的，
+                            不需要在这里添加。
                             
-                            Use the tag "?all" to block all items. This will make it so players
-                            can only pick up mobs with an empty hand.
+                            使用标签 "?all" 来阻止所有物品。这将使得玩家
+                            只能用空手捡起生物。
                             
-                            For items from another mod, add the namespace before the item name.
-                                i.E: "othermod:otheritem"
+                            对于来自其他模组的物品，请在物品名称前添加命名空间。
+                                例如: "othermod:otheritem"
                             """)
                     .onChange(list -> {
                         PickupHandler.BLACKLISTED_MATERIALS_IN_HAND.clear();
--- paper/src/main/java/de/clickism/clickmobs/ClickMobsConfig.java
+++ paper/src/main/java/de/clickism/clickmobs/ClickMobsConfig.java
@@ -36,9 +36,9 @@ public interface ClickMobsConfig {
                             .load());
 
     ConfigOption<Boolean> CHECK_UPDATE =
-            CONFIG.option("check_update", true)
+            CONFIG.option("check_update", false)
                     .description("""
-                            Whether to check for updates on server startup. Strongly Recommended.
+                            是否在服务器启动时检查更新。
                             """);
 
     ConfigOption<Boolean> PER_MOB_PERMISSIONS =
@@ -54,7 +54,7 @@ public interface ClickMobsConfig {
     ConfigOption<Boolean> ENABLE_DISPENSERS =
             CONFIG.option("enable_dispensers", true)
                     .description("""
-                            Whether dispensers can dispense picked up mobs.
+                            发射器是否可以发射捡起的生物。
                             """);
 
     ConfigOption<List<String>> WHITELISTED_MOBS =
@@ -62,55 +62,58 @@ public interface ClickMobsConfig {
                     .listOf(String.class)
                     .header("""
                             ---------------------------------------------------------
-                            In the following section you can whitelist/blacklist mobs.
+                            在以下部分，您可以设置生物的白名单/黑名单。
                             ---------------------------------------------------------
-                            To whitelist/blacklist a vanilla mob, add its entity name.
-                                i.E: "creeper" or "ender_dragon"
+                            要将原版生物加入白名单/黑名单，请添加其实体名称。
+                                例如: "creeper" 或 "ender_dragon"
+                            要将其他模组的生物加入白名单/黑名单，请使用带命名空间的完整标识符。
+                                例如: "othermod:fancy_creeper"
                             ---------------------------------------------------------
-                            You can also use tags (predicates) to whitelist/blacklist mobs with certain properties.
-                            Available tags are:
-                                ?all, ?hostile, ?baby, ?tamed, ?nametagged, ?silent, ?mob
+                            您还可以使用标签（谓词）来根据特定属性设置白名单/黑名单。
+                            可用的标签有：
+                                ?all (所有), ?hostile (敌对), ?baby (幼年), ?tamed (驯服), ?nametagged (命名), ?silent (静音), ?mob (特定生物)
                             
-                            You can (optionally) pass arguments to certain tags:
+                            您可以（可选地）向某些标签传递参数：
                                 - ?nametagged(Dinnerbone)
-                            You can combine multiple tags:
+                            您可以组合多个标签：
                                 - ?tamed ?nametagged
-                            You can negate tags using "not":
+                            您可以使用 "not" 来否定标签：
                                 - not ?hostile
-                            You can use the ?mob tag alongside other tags to use tags on specific mobs:
+                            您可以将 ?mob 标签与其他标签结合使用，以便在特定生物上使用标签。
                                 - ?mob(creeper, zombie) ?nametagged(Friendly!)
-                            You can use the ?customdata tag to target mobs added by other plugins or mobs with
-                            custom data. Please read the wiki for more information on this tag.
                             
-                            Check the wiki for more documentation on tags:
-                            https://clickism.de/docs/clickmobs/tags
+                            查看 wiki 以获取更多关于标签的文档：
+                            https://github.com/Clickism/ClickMobs/wiki/Tags
                             ---------------------------------------------------------
                             """)
                     .description("""
-                            Mobs that are allowed to be picked up.
-                            The whitelist takes precedence over the blacklist.
-                            (Blacklisted mobs included in the whitelist will still be allowed)
+                            允许被捡起的生物。
+                            白名单优先于黑名单。
+                            （包含在白名单中的黑名单生物仍然可以被捡起）
                             """);
 
     ConfigOption<List<String>> BLACKLISTED_MOBS =
             CONFIG.option("blacklisted_mobs", List.of("?hostile", "wither", "ender_dragon"))
                     .listOf(String.class)
                     .description("""
-                            Mobs that are not allowed to be picked up.
+                            不允许被捡起的生物。
                             """);
 
     ConfigOption<List<String>> BLACKLISTED_ITEMS_IN_HAND =
             CONFIG.option("blacklisted_items_in_hand", List.of("lead", "saddle"))
                     .listOf(String.class)
                     .description("""
-                            Items that prevent picking up mobs when held in hand.
-                            Use the item names (material names) of the items.
+                            手持时阻止捡起生物的物品。
+                            使用物品的（完整）标识符。
                             
-                            Harnesses are always blocked for Happy Ghast compatibility
-                            and do not need to be added here.
+                            为了兼容 Happy Ghast，缰绳总是被阻止的，
+                            不需要在这里添加。
                             
-                            Use the tag "?all" to block all items. This will make it so players
-                            can only pick up mobs with an empty hand.
+                            使用标签 "?all" 来阻止所有物品。这将使得玩家
+                            只能用空手捡起生物。
+                            
+                            对于来自其他模组的物品，请在物品名称前添加命名空间。
+                                例如: "othermod:otheritem"
                             """);
 
     ConfigOption<Map<String, Integer>> CUSTOM_MODEL_DATA =
